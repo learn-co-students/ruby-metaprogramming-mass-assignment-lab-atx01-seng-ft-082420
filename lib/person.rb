0fdbe82307e7bdd5dfc6953bceffff2c3fb8d4f6
jlonetree <jlonetree@gmail.com>
2020-09-05 14:35:38 -0500

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,13 @@
+require 'pry'
+
 class Person
-  #your code here
+  attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complextion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
+
+    def initialize(stats)
+      stats.each do |key, value|
+        self.class.attr_accessor(key)
+        self.send(("#{key}="), value)
+      end
+    end
+
 end
